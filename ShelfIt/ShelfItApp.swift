351ce84f7af4e51f47992665c9c8892ea3feb390
guywithhat99 <jami6753@colorado.edu>
2025-10-14 17:39:25 -0600

    init commit

---
+++ ShelfIt/ShelfItApp.swift
@@ -0,0 +1,18 @@
+//
+//  ShelfItApp.swift
+//  ShelfIt
+//
+//  Created by Jack Miller on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ShelfItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
