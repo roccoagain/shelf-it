4dab01bcaeb31ec5d2ed85ac6d02fbf8f6ee3647
guywithhat99 <jami6753@colorado.edu>
2025-10-26 23:07:54 -0600

    added welcome view file

---
+++ ShelfIt/PickerPanel.swift
@@ -0,0 +1,15 @@
+//
+//  PickerPanel.swift
+//  ShelfIt
+//
+//  Created by Jack Miller on 10/26/25.
+//
+
+import SwiftUI
+
+struct PickerPanel: View {
+    let shouldBeShown = false
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
---
+++ ShelfIt/welcomeView.swift
@@ -0,0 +1,29 @@
+//
+//  welcomeView.swift
+//  ShelfIt
+//
+//  Created by Jack Miller on 10/26/25.
+//
+
+import SwiftUI
+
+struct welcomeView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("Welcome to ShelfIt!")
+                    .font(.largeTitle)
+                    .padding()
+                Text("this lets you organize todos.")
+                    .font(.body)
+                    .padding()
+            }
+        }
+        .glassBackgroundEffect()
+            
+    }
+}
+
+#Preview {
+    welcomeView()
+}
